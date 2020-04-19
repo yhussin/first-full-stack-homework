fcb70a508a8adccecce821b7840f36005d0b6615
yhussin <yhussin@gmail.com>
2020-04-18 18:47:47 -0700

    Adds models and views

---
+++ models/Car.js
@@ -0,0 +1,7 @@
+const Car = [
+    {make: 'Toyota', model: 'Corolla', year: 1996},
+    {make: 'Honda', model: 'Civic', year: 2006},
+    {make: 'Nissan', model: 'Altima', year: 2020},
+];
+
+module.exports = Car;
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 const PORT = 4000;
 
+const Car = require('./models/Car')
+
+//ROUTES
 app.get('/', (req, res) => {
     res.send('<h1>Full Stack Homework</h1>');
 });
+
+app.get('/cars', (req, res) => {
+    res.render('index', {
+        cars: Car,
+    });
+});
+
+app.get('/cars/:index', (req, res) => {
+    res.render('show', {
+        car: Car[req.params.index]
+    });
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
